19f8bb8381adfb7abf53c071bef41bdc56d03b43
kala4678 <whiteinsta06@gmail.com>
2026-04-18 18:16:32 -0700

    dark mode

--- global.js
+++ global.js
@@ -16,6 +16,19 @@ let pages = [
   { url: 'resume/', title: 'Resume' },
   { url: 'https://github.com/kala4678', title: 'GitHub' }
 ];
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
   : "/website/";         // GitHub Pages repo name
