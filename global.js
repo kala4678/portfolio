8c3be175fc4b4765f3c329be94d2c64f5088dc6d
kala4678 <whiteinsta06@gmail.com>
2026-04-18 17:27:02 -0700

    links fixed

--- global.js
+++ global.js
@@ -16,11 +16,15 @@ let pages = [
   { url: 'resume/', title: 'Resume' },
   { url: 'https://github.com/kala4678', title: 'GitHub' }
 ];
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
 
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
