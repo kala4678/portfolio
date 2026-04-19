35ebac1bdbb9dc16844cad8ed88bf7826664d1b8
kala4678 <whiteinsta06@gmail.com>
2026-04-18 17:23:14 -0700

    new changes

--- global.js
+++ global.js
@@ -3,3 +3,24 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+// const $$ = (selector) => Array.from(document.querySelectorAll(selector));
+// const navLinks= $$("nav a");
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+// currentLink?.classList.add('current');
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/kala4678', title: 'GitHub' }
+];
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
