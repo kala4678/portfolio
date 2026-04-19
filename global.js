0d2369fdc34378c189960db600fbf9b2fae146ee
kala4678 <whiteinsta06@gmail.com>
2026-04-18 17:51:40 -0700

    external links n links highlight

--- global.js
+++ global.js
@@ -29,5 +29,15 @@ for (let p of pages) {
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
+
+    // Highlight current page
+  a.classList.toggle(
+    "current",
+    a.host === location.host && a.pathname === location.pathname
+  );
+  // Open external links in new tab
+  a.toggleAttribute("target", a.host !== location.host);
+  
+
   nav.append(a);
 }
