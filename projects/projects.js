1606ac61b09e81043270969c4fb78c741d55dd98
kala4678 <whiteinsta06@gmail.com>
2026-04-24 12:57:01 -0700

    titles update automatically

--- projects/projects.js
+++ projects/projects.js
@@ -2,4 +2,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 console.log(projects);
 const projectsContainer = document.querySelector('.projects');
+// NEW: update title with count
+const title = document.querySelector('.projects-title');
+title.textContent = `Projects (${projects.length})`;
 renderProjects(projects, projectsContainer, 'h2');
