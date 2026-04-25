a866bad05179a52d7224b849d374cba05ad17bf1
kala4678 <whiteinsta06@gmail.com>
2026-04-24 17:34:17 -0700

    api 2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
+
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
-const githubData = await fetchGitHubData('kala4678'); 
+
+const githubData = await fetchGitHubData('kala4678');
 
 const profileStats = document.querySelector('#profile-stats');
 
