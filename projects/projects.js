e74d964e59d25b6c96d485cd1ef122b5da890fae
kala4678 <whiteinsta06@gmail.com>
2026-05-01 23:02:49 -0700

    new pie changes

--- projects/projects.js
+++ projects/projects.js
@@ -1,8 +1,26 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 const projects = await fetchJSON('../lib/projects.json');
-console.log(projects);
+
 const projectsContainer = document.querySelector('.projects');
-// NEW: update title with count
 const title = document.querySelector('.projects-title');
-title.textContent = `Projects (${projects.length})`;
+title.textContent = `${projects.length} Projects`;
 renderProjects(projects, projectsContainer, 'h2');
+
+// Pie chart
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});
