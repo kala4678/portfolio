b259a8148198ae4c615992f9352e8500acae9b46
kala4678 <whiteinsta06@gmail.com>
2026-05-08 16:46:45 -0700

    stats edits

--- meta/main.js
+++ meta/main.js
@@ -47,6 +47,50 @@ function processCommits(data) {
 
     });
 }
+function renderCommitInfo(data, commits) {
+  // Create the dl element
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  // Add total LOC
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  // Add total commits
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  // Total LOC
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+ 
+  // Total commits
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+ 
+  // Number of files
+  dl.append('dt').text('Files');
+  dl.append('dd').text(d3.group(data, (d) => d.file).size);
+ 
+  // Max file length
+  const fileLengths = d3.rollups(
+    data,
+    (v) => d3.max(v, (v) => v.line),
+    (d) => d.file,
+  );
+  dl.append('dt').text('Longest file (lines)');
+  dl.append('dd').text(d3.max(fileLengths, (d) => d[1]));
+ 
+  // Average file length
+  dl.append('dt').text('Avg file length (lines)');
+  dl.append('dd').text(Math.round(d3.mean(fileLengths, (d) => d[1])));
+ 
+  // Max depth
+  dl.append('dt').text('Max depth');
+  dl.append('dd').text(d3.max(data, (d) => d.depth));
+}
+ 
+
+
 let data = await loadData();
 let commits = processCommits(data);
 console.log(commits);
