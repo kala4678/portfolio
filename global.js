705f57a60ac2d877b3923069d9e8484edd03599f
kala4678 <whiteinsta06@gmail.com>
2026-05-08 16:01:29 -0700

    meta

--- global.js
+++ global.js
@@ -14,6 +14,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'https://github.com/kala4678', title: 'GitHub' }
 ];
 document.body.insertAdjacentHTML(
